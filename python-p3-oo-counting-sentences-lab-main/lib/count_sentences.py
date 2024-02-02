6af370771940d9b035f41359c1737c3580a4b1e4
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-02 13:55:11 -0500

    python-p3-oo-counting-sentences-lab/
    python-p3-oo-counting-sentences-lab/

--- python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
+++ python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
@@ -1,50 +1,22 @@
 #!/usr/bin/env python3
 
-#!/usr/bin/env python3
-
-import ipdb
-
 class MyString:
+    def __init__(self, value=""):
+        self.value = value
 
-  def __init__(self, value = ""):
-    self._value = value if type(value) == str else None
+    def is_sentence(self):
+        return self.value.endswith(".")
 
-  def get_value(self):
-    return self._value
+    def is_question(self):
+        return self.value.endswith("?")
 
-  def set_value(self, value):
-    if type(value) == str:
-      self._value = value
-    else:
-      print("The value must be a string.")
-      return None
+    def is_exclamation(self):
+        return self.value.endswith("!")
 
-  def ends_with(self, sentence, end_char):
-    if sentence[-1] == end_char:
-      return True
-    else: 
-      return False
-
-  def is_sentence(self):
-    return self.ends_with(self._value, '.')
- 
-  def is_question(self):
-    return self.ends_with(self._value, '?')
-
-  def is_exclamation(self):
-    return self.ends_with(self._value, '!')
-
-  def count_sentences(self):
-    counter = 0
-    split_sentence = self._value.split(' ')
-    for sentence in split_sentence:
-      if(len(sentence) > 0):
-        string = MyString(sentence)
-        counter += 1 if string.is_sentence() or string.is_question() or string.is_exclamation() else 0
-
-    return counter
-
-  value = property(get_value, set_value)
-
-  pass
- 
+    def count_sentences(self):
+        if not self.value:
+            return 0
+        sentences = self.value.split(".")
+        sentences = [sentence for sentence in sentences if sentence.strip()]
+        return len(sentences)
+  
