d8031ef99db8473619afb4acd8805f39794199b8
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-02 14:13:21 -0500

    python-p3-oo-counting-sentences-lab/
    python-p3-oo-counting-sentences-lab/

--- python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
+++ python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
 
 class MyString:
-    def __init__(self, value=""):
-        self.value = value
+  def __init__(self, value=''):
+    self.value = value
 
-    def get_value(self):
-        return self._value
+  def get_value(self):
+    return self._value
+  
+  def set_value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print('The value must be a string.')
 
-    def set_value(self, value):
-        if (type(value) == str):
-            self._value = value
-        else:
-            print("The value must be a string.")
+  value = property(get_value, set_value)
 
-    value = property(get_value, set_value)
-
-    def is_sentence(self):
-        return True if self._value[-1] == "." else False
-
-    def is_question(self):
-        return True if self._value[-1] == "?" else False
-
-    def is_exclamation(self):
-        return True if self._value[-1] == "!" else False
-
-    def count_sentences(self):
-        count = 0
-        for word in self._value.split():
-            if (word[-1] == "." or word[-1] == "!" or word[-1] == "?"):
-                count += 1
-        return count
+  def is_sentence(self):
+    return self.value[-1] == '.'
+  
+  def is_question(self):
+    return self.value[-1] == '?'
+  
+  def is_exclamation(self):
+    return self.value[-1] == '!'
+  
+  def count_sentences(self):
+    if self.value == '':
+      return 0
+    else:
+      str2 = self.value.replace('!', '^', 1)
+      str3 = str2.replace('.', '^', 1)
+      str4 = str3.replace('?', '^', 1)
+      split_str = str4.split('^')
+      return len(split_str)
