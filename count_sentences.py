a1dfa5860ba55d14eefdd299f31764ac7f7b106f
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-02 14:25:34 -0500

    python-p3-oo-counting-sentences-lab/
    python-p3-oo-counting-sentences-lab/

---
+++ count_sentences.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+class MyString:
+    def __init__(self, value=''):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            raise ValueError("The value must be a string.")
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = [sentence for sentence in self.value.split('.') if sentence.strip()]
+        return len(sentences)
