2b11bb4c4aca38c339ffb521cd35572fbe185af7
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-02 14:03:18 -0500

    python-p3-oo-counting-sentences-lab/
    python-p3-oo-counting-sentences-lab/

--- python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
+++ python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
@@ -1,50 +1,34 @@
 #!/usr/bin/env python3
 
-#!/usr/bin/env python3
-
-import ipdb
-
 class MyString:
-
-  def __init__(self, value = ""):
-    self._value = value if type(value) == str else None
-
-  def get_value(self):
-    return self._value
+  
+  def __init__(self, value = ''):
+    self.value = value
 
   def set_value(self, value):
-    if type(value) == str:
+    if isinstance(value, str):
       self._value = value
     else:
-      print("The value must be a string.")
-      return None
-
-  def ends_with(self, sentence, end_char):
-    if sentence[-1] == end_char:
-      return True
-    else: 
-      return False
-
-  def is_sentence(self):
-    return self.ends_with(self._value, '.')
- 
-  def is_question(self):
-    return self.ends_with(self._value, '?')
-
-  def is_exclamation(self):
-    return self.ends_with(self._value, '!')
-
-  def count_sentences(self):
-    counter = 0
-    split_sentence = self._value.split(' ')
-    for sentence in split_sentence:
-      if(len(sentence) > 0):
-        string = MyString(sentence)
-        counter += 1 if string.is_sentence() or string.is_question() or string.is_exclamation() else 0
-
-    return counter
-
+      print('The value must be a string.')
+  
+  def get_value(self):
+    return self._value
+  
   value = property(get_value, set_value)
 
-  pass
- 
+  def is_sentence(self):
+    return True if self._value.endswith('.') else False
+
+  def is_question(self):
+    return True if self._value.endswith('?') else False
+
+  def is_exclamation(self):
+    return True if self._value.endswith('!') else False
+  
+  def count_sentences(self):
+    i = 0
+    for word in self._value.split():
+      if word.endswith('.') or word.endswith('!') or word.endswith('?'):
+        i += 1
+    return i
+  
