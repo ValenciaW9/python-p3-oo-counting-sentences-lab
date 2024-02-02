61964e0eb6e8850e0ebb0301a3357a852dc37bcd
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-02 14:20:11 -0500

    python-p3-oo-counting-sentences-lab/
    python-p3-oo-counting-sentences-lab/

--- python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
+++ python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
@@ -1,35 +1,35 @@
 #!/usr/bin/env python3
 
 class MyString:
-  def __init__(self, value=''):
-    self.value = value
 
-  def get_value(self):
-    return self._value
-  
-  def set_value(self, value):
-    if type(value) == str:
-      self._value = value
-    else:
-      print('The value must be a string.')
+    def __init__(self, value=''):
+        self._value = value
 
-  value = property(get_value, set_value)
+    def get_value(self):
+        return self._value
 
-  def is_sentence(self):
-    return self.value[-1] == '.'
-  
-  def is_question(self):
-    return self.value[-1] == '?'
-  
-  def is_exclamation(self):
-    return self.value[-1] == '!'
-  
-  def count_sentences(self):
-    if self.value == '':
-      return 0
-    else:
-      str2 = self.value.replace('!', '^', 1)
-      str3 = str2.replace('.', '^', 1)
-      str4 = str3.replace('?', '^', 1)
-      split_str = str4.split('^')
-      return len(split_str)
+    def set_value(self, stringVal):
+        if (type(stringVal) == str):
+            self._value = stringVal
+        else:
+            print('The value must be a string.')
+
+    value = property(get_value, set_value)
+
+    def is_sentence(self):
+        return self._value.endswith(".")
+
+    def is_question(self):
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        value = self.value
+        for punc in ['!', '?']:
+            value = value.replace(punc, '.')
+
+        sentences = [s for s in value.split('.') if s]
+
+        return len(sentences)
