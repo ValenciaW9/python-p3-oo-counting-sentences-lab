ca296c5be5e4d1465e7afd868217f2b13c08bcc4
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-02 14:21:57 -0500

    python-p3-oo-counting-sentences-lab/
    python-p3-oo-counting-sentences-lab/

--- python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
+++ python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
@@ -1,35 +1,34 @@
 #!/usr/bin/env python3
 
 class MyString:
-
-    def __init__(self, value=''):
+    def __init__(self, value = ''):
         self._value = value
 
     def get_value(self):
-        return self._value
-
-    def set_value(self, stringVal):
-        if (type(stringVal) == str):
-            self._value = stringVal
+      return self._value
+    
+    def set_value(self, value):
+        if isinstance(value, str):
+            self._page_count = value
         else:
-            print('The value must be a string.')
+            print("The value must be a string.")
 
     value = property(get_value, set_value)
 
     def is_sentence(self):
-        return self._value.endswith(".")
+        return self._value.endswith('.')
 
     def is_question(self):
-        return self._value.endswith("?")
+        return self._value.endswith('?')
 
     def is_exclamation(self):
-        return self._value.endswith("!")
-
+        return self._value.endswith('!')
+    
     def count_sentences(self):
-        value = self.value
-        for punc in ['!', '?']:
-            value = value.replace(punc, '.')
-
-        sentences = [s for s in value.split('.') if s]
-
-        return len(sentences)
+      value = self.value
+      for punctuation in ['!', '?']:
+        value = value.replace(punctuation, '.')
+    
+      sentences = [s for s in value.split('.') if s]
+    
+      return len(sentences)
