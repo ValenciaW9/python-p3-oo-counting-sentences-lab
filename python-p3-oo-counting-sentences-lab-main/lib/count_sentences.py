da65b5b641f9889ed518a2fcd3ccdc9209c7af2a
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-02-02 13:57:18 -0500

    python-p3-oo-counting-sentences-lab/
    python-p3-oo-counting-sentences-lab/

--- python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
+++ python-p3-oo-counting-sentences-lab-main/lib/count_sentences.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python3
+import re
+
 
 class MyString:
-    def __init__(self, value=""):
-        self.value = value
+    def __init__(self, value=''):
+        self._value = None
+        self.set_value(value)
+
+    def get_value(self):
+        return self._value
+
+    def set_value(self, new_value):
+        if isinstance(new_value, str):
+            self._value = new_value
+        else:
+            print("The value must be a string.")
 
     def is_sentence(self):
-        return self.value.endswith(".")
+        return self._value.endswith('.')
 
     def is_question(self):
-        return self.value.endswith("?")
+        return self._value.endswith('?')
 
     def is_exclamation(self):
-        return self.value.endswith("!")
+        return self._value.endswith('!')
 
     def count_sentences(self):
-        if not self.value:
-            return 0
-        sentences = self.value.split(".")
+        pattern = r'[.!?]'
+        sentences = re.split(pattern, self._value)
+        print(sentences)
         sentences = [sentence for sentence in sentences if sentence.strip()]
-        return len(sentences)
-  
+        return(len(sentences))
+
+    value = property(get_value, set_value)
+
+
+string = MyString("")
+string.value = "This is a string! It has three sentences. Right?"
+string.count_sentences()
